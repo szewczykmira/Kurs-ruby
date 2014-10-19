aa534bb5a95afe219639643f9bd65ba8ab7c2797
szewczykmira <szewczyk.mira@gmail.com>
2014-10-19 16:09:43 +0200

    4th assigment from week 3 added

---
+++ Zestaw3/Zadanie4.rb
@@ -0,0 +1,24 @@
+__author__ = "Mira"
+def rozklad(n)
+  if n < 1
+    return [n]
+  end
+  _czynnik = 2
+  temp = []
+  zliczacz = 0
+  while _czynnik <= n
+    if n % _czynnik == 0
+      zliczacz +=1
+      n /= _czynnik
+    else
+      if zliczacz != 0
+        temp.push([_czynnik, zliczacz])
+        zliczacz = 0
+      end
+      _czynnik += 1
+    end
+  end
+  temp.push([[_czynnik, zliczacz]])
+end
+
+print rozklad(756)
